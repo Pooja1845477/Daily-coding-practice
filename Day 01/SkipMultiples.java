ef92274bb09dfffb8604a20905feb914102247fe
Pooja Kumari <pk5130983@gmail.com>
2026-01-22 22:43:28 +0530

    Day 01: Added SkipMultiples problem

---
+++ Day 01/SkipMultiples.java
@@ -0,0 +1,16 @@
+//package Day 01;
+
+//public class SkipMultiples {
+    
+import java.util.*;
+import java.io.*;
+public class SkipMultiples{
+    public static void main(String args[]){
+        int i;
+        for(i=0;i<=100;i++){
+            if(i%5==0)
+                continue;
+        
+        System.out.print(i+" ");
+    }}
+}
